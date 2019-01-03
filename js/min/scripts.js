cbddd12c415cc3e5b60731a638d053059e007787
Matt Moreno <mattmoreno@gmail.com>
2019-01-02 21:49:06 -0600

    Game 38 vs Orlando

--- js/min/scripts.js
+++ js/min/scripts.js
@@ -20111,7 +20111,7 @@ $( document ).ready(function() {
 		var game = data.gscd.g;
 
     // CHANGE THIS NUMBER
-		var gamenum = 35;
+		var gamenum = 38;
 
 		gamenum = gamenum - 1;
 		var getfinal = "Final";
--- js/scripts.js
+++ js/scripts.js
@@ -35,7 +35,7 @@ $( document ).ready(function() {
 		var game = data.gscd.g;
 
     // CHANGE THIS NUMBER
-		var gamenum = 35;
+		var gamenum = 38;
 
 		gamenum = gamenum - 1;
 		var getfinal = "Final";
