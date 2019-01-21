554970141a8350fa806d617a04d17d195ec2e571
Matt Moreno <mattmoreno@gmail.com>
2019-01-21 09:21:17 -0600

    Game 46 vs. Cleveland

--- js/min/scripts.js
+++ js/min/scripts.js
@@ -20111,7 +20111,7 @@ $( document ).ready(function() {
 		var game = data.gscd.g;
 
     // CHANGE THIS NUMBER
-		var gamenum = 44;
+		var gamenum = 47;
 
 		gamenum = gamenum - 1;
 		var getfinal = "Final";
